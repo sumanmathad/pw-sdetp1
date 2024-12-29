2144a1e1a65f7e12df94daa2b3c9167fb7621e8e
suman.mathad <suman.mathad@oracle.com>
2024-12-29 12:43:42 +0530

    added tests to locators file

--- tests/locators.spec.js
+++ tests/locators.spec.js
@@ -1,5 +1,5 @@
-//const {test, expect} = require('@playwright/test');
-import {test, expect} from '@playwright/test'
+const {test, expect} = require('@playwright/test');
+//import {test, expect} from '@playwright/test'
 
 test('Enter user credentials and Login', async({page})=>{
 
@@ -14,6 +14,36 @@ test('Enter user credentials and Login', async({page})=>{
 
 })
 
+test('Verify burger menu item list', async({page})=>{
+
+    await page.goto("https://www.saucedemo.com/");
+
+    await page.locator('#user-name').fill("standard_user");
+    await page.fill('#password',"secret_sauce");
+    await page.click('#login-button');
+
+    await page.locator('#react-burger-menu-btn').click();
+
+    await expect(page.getByRole('link',{name: 'All Items'})).toBeVisible();
+
+
+});
+
+
+test('print the names of all products', async({page})=>{
+    await page.goto("https://www.saucedemo.com/");
+
+    await page.locator('#user-name').fill("standard_user");
+    await page.fill('#password',"secret_sauce");
+    await page.click('#login-button');
+
+    const productNameArray = await page.$$('div[data-test="inventory-item"] .inventory_item_description a');
+
+    for ( const names of productNameArray){
+        console.log(await names.textContent());
+    }
+
+});
 
 
 
